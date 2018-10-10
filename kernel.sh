45c50f7fc780d7704762de797e39d6a79e329e9c
idkwhoiam322 <snkm1999@gmail.com>
2018-10-10 19:49:03 +0530

    Add compilation time calculator and weebify script

--- kernel.sh
+++ kernel.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 cd ..
 export KBUILD_COMPILER_STRING="$($(pwd)/clang/clang-r328903/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')";
-
 curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build started for branch $(git rev-parse --abbrev-ref HEAD) using Clang 7.0.2!" -d chat_id=$CHAT_ID
 curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Latest Commits:
 $(git log --pretty=format:'%h : %s' -{1..5})" -d chat_id=$CHAT_ID
 rm -rf out
 mkdir -p out
+
+#	Let's compile this mess
 make O=out ARCH=arm64 test_defconfig
 chmod +x -R $(pwd)/
-ZIPNAME="WeebKernelOOS_$(date '+%Y-%m-%d_%H:%M:%S').zip"
-make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r328903/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"	| tee $LOGFILE
 
-#Failure
+#	Compile the Kernel
+START=$(date +"%s")
+time make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r328903/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"	| tee $LOGFILE
+END=$(date +"%s")
+DIFF=$((END - START))
+# START, END and DIFF variables to calculate rough compilation time!
+
+#	Failure
 EXITCODE=$?
 if [ $EXITCODE -ne 0 ]; then 
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build Failed! Check log file <code>$LOGFILE</code>" -d chat_id=$CHAT_ID
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Senpai, I hate to tell you but... git commit die!
+Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds to fail miserably!
+Check log file <code>$LOGFILE</code>" -d chat_id=$CHAT_ID
 curl -F chat_id="$CHAT_ID" -F document=@"$LOGFILE" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
 exit 1;
 fi
 
-#Success
+#	Success
 rm -rf $(pwd)/anykernel/ramdisk/modules/wlan.ko
 rm -rf $(pwd)/anykernel/kernels/oos/Image.gz-dtb
 chmod +x -R $(pwd)/out
+
+#	Preparing Kernel ZIP
 mkdir anykernel/kernels
 mkdir anykernel/kernels/oos
 mkdir anykernel/ramdisk/modules
@@ -32,7 +42,14 @@ cp $(pwd)/out/drivers/staging/qcacld-3.0/wlan.ko $(pwd)/anykernel/ramdisk/module
 $(pwd)/gcc/bin/aarch64-linux-android-strip --strip-unneeded $(pwd)/anykernel/ramdisk/modules/wlan.ko
 find $(pwd)/anykernel/ramdisk/modules -name '*.ko' -exec $(pwd)/out/scripts/sign-file sha512 $(pwd)/out/certs/signing_key.pem $(pwd)/out/certs/signing_key.x509 {} \;
 cd $(pwd)/anykernel
+ZIPNAME="WeebKernelOOS_$(date '+%Y-%m-%d_%H:%M:%S').zip"
 zip -r9 $ZIPNAME * -x README.md $ZIPNAME
+
+#	Time to push the Kernel ZIP
 cd ..
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Download:" -d chat_id=$CHAT_ID
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build Success Oniisama!
+The build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds to compile successfully!!
+Uploading Kernel zip file here now!! 
+	~(^.^)~" -d chat_id=$CHAT_ID
 curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/anykernel/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+curl -F chat_id="$CHAT_ID" -F document=@"CAADBQADIwAD8IPPFYsobXzJMz7mAg" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
