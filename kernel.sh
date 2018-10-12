90f62feb5aa27c2eb3f5492a5f46aca6372abbbf
idkwhoiam322 <snkm1999@gmail.com>
2018-10-12 21:33:06 +0530

    update build script to support both oxygen and other treble ROMs

--- kernel.sh
+++ kernel.sh
@@ -8,13 +8,17 @@ rm -rf out
 mkdir -p out
 
 #	Let's compile this mess
-make O=out ARCH=arm64 test_defconfig
+#
+#	Time for OxygenOS Treble
+#
+make O=out ARCH=arm64 weeb_defconfig
 chmod +x -R $(pwd)/
 
 #
 #	Compile the Kernel
 #
-# START, END and DIFF variables to calculate rough compilation time!
+
+# START, END and DIFF variables to calculate rough total compilation time!
 
 START=$(date +"%s")
 make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r328903/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"	| tee $LOGFILE
@@ -22,7 +26,7 @@ EXITCODE=$?
 #	Failure
 if [ $EXITCODE -ne 0 ]; then 
 curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Senpai, I hate to tell you but... git commit die!
-Build took so much time to fail miserably!
+Build took so much time to fail building for OxygenOS miserably!
 Check log file <code>$LOGFILE</code>" -d chat_id=$CHAT_ID
 curl -F chat_id="$CHAT_ID" -F document=@"$LOGFILE" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
 curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker \
@@ -30,8 +34,7 @@ curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker \
 	-d chat_id=${CHAT_ID} >> /dev/null
 exit 1;
 fi
-END=$(date +"%s")
-DIFF=$((END - START))
+
 
 
 
@@ -48,8 +51,40 @@ cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/oos/
 cp $(pwd)/out/drivers/staging/qcacld-3.0/wlan.ko $(pwd)/anykernel/ramdisk/modules
 $(pwd)/gcc/bin/aarch64-linux-android-strip --strip-unneeded $(pwd)/anykernel/ramdisk/modules/wlan.ko
 find $(pwd)/anykernel/ramdisk/modules -name '*.ko' -exec $(pwd)/out/scripts/sign-file sha512 $(pwd)/out/certs/signing_key.pem $(pwd)/out/certs/signing_key.x509 {} \;
+
+#
+#	Time for Custom Treble
+#
+rm -rf out
+mkdir -p out
+make O=out ARCH=arm64 weebcustom_defconfig
+chmod +x -R $(pwd)/
+make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r328903/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"	| tee $LOGFILE
+EXITCODE=$?
+
+#	Failure
+if [ $EXITCODE -ne 0 ]; then 
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Senpai, I hate to tell you but... git commit die!
+Build took so much time to fail building for Treble ROMs miserably!
+Check log file <code>$LOGFILE</code>" -d chat_id=$CHAT_ID
+curl -F chat_id="$CHAT_ID" -F document=@"$LOGFILE" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendSticker \
+	-d sticker="CAADBQADUBwAAsZRxhXTwSK4KP5DpwI" \
+	-d chat_id=${CHAT_ID} >> /dev/null
+exit 1;
+fi
+
+END=$(date +"%s")
+DIFF=$((END - START))
+
+#	Success
+mkdir anykernel/kernels/Custom
+cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/custom/
+
+
+#ReZIP the Kernel
 cd $(pwd)/anykernel
-ZIPNAME="WeebKernelOOS_$(date '+%Y-%m-%d_%H:%M:%S').zip"
+ZIPNAME="WeebKernel-Treble_$(date '+%Y-%m-%d_%H:%M:%S').zip"
 zip -r9 $ZIPNAME * -x README.md $ZIPNAME
 
 #	Time to push the Kernel ZIP
