a5a3e5d35b9116a54d9e50fc17e93738d01b783e
whoknowswhoiam <snkm1999@gmail.com>
2018-10-25 14:47:16 +0530

    Reduce Telegram spam

--- kernel.sh
+++ kernel.sh
@@ -2,9 +2,9 @@
 cd ..
 
 export KBUILD_COMPILER_STRING="$($(pwd)/clang/clang-r328903/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')";
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build started for branch $(git rev-parse --abbrev-ref HEAD) using Clang 7.0.2!" -d chat_id=$CHAT_ID
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Latest Commits:
-$(git log --pretty=format:'%h : %s' -{1..5})" -d chat_id=$CHAT_ID
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build started for branch $(git rev-parse --abbrev-ref HEAD) using Clang 7.0.2!
+Latest Commits:
+$(git log --pretty=format:'%h : %s' -1)" -d chat_id=$CHAT_ID
 rm -rf out
 mkdir -p out
 
