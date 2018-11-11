d7a371e6d56e3b66cb9347edbfc7035972a40da9
idkwhoiam322 <snkm1999@gmail.com>
2018-11-11 10:54:29 +0530

    refresh script clean up to 2 telegram posts provide device and compiler information add monospace unify zip again
    bah

--- update.sh
+++ update.sh
@@ -17,15 +17,13 @@ make O=out ARCH=arm64 weeb_defconfig
 
 START=$(date +"%s")
 make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r344140/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"
-END=$(date +"%s")
-DIFF=$((END - START))
 
 #	Success
 #	Remove any residue
 rm -rf $(pwd)/anykernel/ramdisk/modules/wlan.ko
 rm -rf $(pwd)/anykernel/kernels/oos/Image.gz-dtb
 
-#	Preparing Kernel ZIP
+#	Preparing Kernel ZIP for OxygenOS
 mkdir anykernel/kernels
 mkdir anykernel/kernels/oos
 mkdir anykernel/ramdisk/modules
@@ -39,23 +37,7 @@ cd $(pwd)/anykernel
 #	We don't ned non treble anykernel
 rm -rf nontreble.sh
 mv treble.sh anykernel.sh
-
-#	Name and push zip
-ZIPNAME="WEEB_KERNEL_OOSr$SEMAPHORE_BUILD_NUMBER.zip"
-zip -r9 $ZIPNAME * -x README.md $ZIPNAME
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Kernel: Weeb Kernel
-Type: BETA
-Revision: $REVISION
-ROM Support: OxygenOS
-Compilation Time: $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds
-Uploading...." -d chat_id=$CHAT_ID
-curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
-rm -rf $ZIPNAME
-rm -rf kernels/oos
-rm -rf ramdisk/modules
 cd ..
-
-
 
 #
 #	Time for Custom Treble
@@ -66,22 +48,23 @@ make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r344140/bin/clang"
 END=$(date +"%s")
 DIFF=$((END - START))
 
-#	Success
+#	Preparing Kernel ZIP for Custom Treble ROMs
 mkdir anykernel/kernels/custom
 cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/custom/
 
 #	Name and push zip
 cd $(pwd)/anykernel
-ZIPNAME="WEEB_KERNEL_TREBLEr$SEMAPHORE_BUILD_NUMBER.zip"
+ZIPNAME="WEEB_KERNEL_TREBLE_r$SEMAPHORE_BUILD_NUMBER.zip"
 zip -r9 $ZIPNAME * -x README.md $ZIPNAME
 
-#	Time to push the Custom ROM Treble Kernel ZIP
 curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Kernel: Weeb Kernel
-Type: BETA
-Revision: $REVISION
-ROM Support: Custom Treble ROMs
-Compilation Time: $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds
-Uploading...." -d chat_id=$CHAT_ID
+Type: <code>BETA</code>
+Device: <code>OnePlus 5/T<code>
+Compiler: <code>Clang 8.0.3</code>
+Revision: <code>$REVISION</code>
+ROM Support: <code>Treble ROMs (Custom + OxygenOS)</code>
+Compilation Time: <code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</code>
+<b>Uploading....</b>" -d chat_id=$CHAT_ID
 curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
 # Extra line here for OCD
 
