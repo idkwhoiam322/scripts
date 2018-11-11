4975f1df97796a45275781323447ea41fdf863c7
idkwhoiam322 <snkm1999@gmail.com>
2018-11-11 13:05:50 +0530

    fix telegram for realz and add psuedo build authenticity check

--- update.sh
+++ update.sh
@@ -6,11 +6,9 @@ curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Ke
 Type: <code>BETA</code>
 Device: <code>OnePlus 5/T</code>
 Compiler: <code>Clang 8.0.3</code>
-Latest Commit: <code>$REVISION</code>
-Build Version: <code>r$SEMAPHORE_BUILD_NUMBER</code>
-ROM Support: <code>Treble ROMs (Custom + OxygenOS)</code>
-Compilation Time: <code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</code>
-<i>Building....</i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
+Latest Commit: <code>$(git log --pretty=format:'%h : %s' -1)</code>
+ROM Support: <code>Treble ROMs (Custom and OxygenOS)</code>
+<i>Build started....</i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
 #	Let's compile this mess
 #
 #	Time for OxygenOS Treble
@@ -64,9 +62,16 @@ cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/custom/
 cd $(pwd)/anykernel
 ZIPNAME="WEEB_KERNEL_TREBLE_r$SEMAPHORE_BUILD_NUMBER.zip"
 zip -r9 $ZIPNAME * -x README.md $ZIPNAME
-
-curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Kernel: <i>Uploading....<i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
-curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+CHECKER=$(ls -l $ZIPNAME | awk '{print $5}')
+if (($((CHECKER / 1048576)) > 5));
+then
+	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build Version: <code>r$SEMAPHORE_BUILD_NUMBER</code>
+	Compilation Time: <code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</code>
+	<i>Uploading....</i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
+	curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+else
+	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="The compiler decides to scream at @idkwhoiam322" -d chat_id=$CHAT_ID	
+fi;
 # Extra line here for OCD
 
 # Oh there were 2 lines
