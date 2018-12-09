bcc9bb220738e546da715aa25f202d16f068bcaa
idkwhoiam322 <snkm1999@gmail.com>
2018-12-09 22:42:21 +0530

    rename test script to main script

--- kernel.sh
+++ kernel.sh
@@ -1,105 +1,108 @@
 #!/bin/bash
 cd ..
+# PREPPING
 
-export KBUILD_COMPILER_STRING="$($(pwd)/clang/clang-r346389/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')";
+# Set COMPILERE
+if [[ "$@" =~ "gcc" ]]; then
+	export COMPILER=GCC
+else
+	export COMPILER=CLANG
+fi
+# remove any old residue
+rm -rf $(pwd)/anykernel/ramdisk/modules/wlan.ko
+rm -rf $(pwd)/anykernel/kernels/oos/Image.gz-dtb
+rm -rf $(pwd)/anykernel/kernels/custom/Image.gz-dtb
 
-if [[ ${SEMAPHORE_PROJECT_NAME} == *"oostest"* ]]; then 
+# Make common kernel folder
+mkdir anykernel/kernels
+
+# How much kebabs we need? Kanged from @raphielscape :)
+if [[ -z "${KEBABS}" ]]; then
+	COUNT="$(grep -c '^processor' /proc/cpuinfo)"
+	export KEBABS="$((COUNT * 2))"
+fi
+
+export ARCH=arm64
+
+if [[ ${COMPILER} == *"CLANG"* ]]; then
+
+	
+	export KBUILD_COMPILER_STRING="$($(pwd)/clang/clang-r346389b/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')";
+	export STRIP=$(pwd)/gcc/bin/aarch64-linux-android-strip
+	export CC="$(pwd)/clang/clang-r346389b/bin/clang"
+	export CLANG_TRIPLE=aarch64-linux-gnu-
+	export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"
+
+		if [[ ${SEMAPHORE_PROJECT_NAME} == *"oostest"* ]]; then 
+			export DEFCONFIG=weeb_defconfig
+			export BUILDFOR=oos
+			export ZIPNAME="weebkernel_oos_r$SEMAPHORE_BUILD_NUMBER.zip"
+			mkdir anykernel/kernels/oos
+			mkdir anykernel/ramdisk/modules
+		fi
+
+		if [[ ${SEMAPHORE_PROJECT_NAME} == *"customtest"* ]]; then
+			export DEFCONFIG=weebcustom_defconfig
+			export BUILDFOR=custom
+			export ZIPNAME="weebkernel_custom_r$SEMAPHORE_BUILD_NUMBER.zip"
+			mkdir anykernel/kernels/custom
+		fi
+fi
+
+if [[ ${COMPILER} == *"GCC"* ]]; then
+	export CROSS_COMPILE=$(pwd)/gcc/bin/aarch64-opt-linux-android-
+	export STRIP=$(pwd)/gcc/bin/aarch64-opt-linux-android-strip
+	export DEFCONFIG=weebcustom_defconfig
+	export BUILDFOR=custom
+	export ZIPNAME="Custom_GCC_r$SEMAPHORE_BUILD_NUMBER.zip"
+	mkdir anykernel/kernels/custom
+fi
+
+# Telegram Post to CI channel
 curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Kernel: <code>Weeb Kernel</code>
 Type: <code>BETA</code>
 Device: <code>OnePlus 5/T</code>
-Compiler: <code>Clang 8</code>
+Compiler: <code>$COMPILER</code>
 Branch: <code>$(git rev-parse --abbrev-ref HEAD)</code>
 Latest Commit: <code>$(git log --pretty=format:'%h : %s' -1)</code>
-ROM Support: <code>Treble ROMs (Custom and OxygenOS)</code>
+ROM Support: <code>$BUILDFOR</code>
 <i>Build started....</i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
 
-# tmpfs bes
-	mkdir -pv out
-	sudo mount -t tmpfs -o size=4g tmpfs out
-	sudo chown "${USER}" out/ -R
-
-
-#	Let's compile this mess
-#
-#
-#	Time for OxygenOS Treble
-#
+# compilation
 START=$(date +"%s")
-make O=out ARCH=arm64 weeb_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r346389/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"
-END=$(date +"%s")
-DIFF=$((END - START))
-#	Success
-#	Remove any residue
-rm -rf $(pwd)/anykernel/ramdisk/modules/wlan.ko
-rm -rf $(pwd)/anykernel/kernels/oos/Image.gz-dtb
-
-#	Preparing Kernel ZIP for OxygenOS
-mkdir anykernel/kernels
-mkdir anykernel/kernels/oos
-mkdir anykernel/ramdisk/modules
-cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/oos/
-cp $(pwd)/out/drivers/staging/qcacld-3.0/wlan.ko $(pwd)/anykernel/ramdisk/modules
-$(pwd)/gcc/bin/aarch64-linux-android-strip --strip-unneeded $(pwd)/anykernel/ramdisk/modules/wlan.ko
-find $(pwd)/anykernel/ramdisk/modules -name '*.ko' -exec $(pwd)/out/scripts/sign-file sha512 $(pwd)/out/certs/signing_key.pem $(pwd)/out/certs/signing_key.x509 {} \;
-fi
-
-
-if [[ ${SEMAPHORE_PROJECT_NAME} == *"customtest"* ]]; then 
-#
-#	Time for Custom Treble
-#
-START=$(date +"%s")
-make O=out ARCH=arm64 weebcustom_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r346389/bin/clang" CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"
+make O=out ARCH=arm64 $DEFCONFIG
+make -j${KEBABS} O=out
 END=$(date +"%s")
 DIFF=$((END - START))
 
-#	Preparing Kernel ZIP for Custom Treble ROMs
-mkdir anykernel/kernels
-mkdir anykernel/kernels/custom
-cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/custom/
+# prepare zip for oos
+if [[ ${BUILDFOR} == *"oos"* ]]; then
+	cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/oos/
+	cp $(pwd)/out/drivers/staging/qcacld-3.0/wlan.ko $(pwd)/anykernel/ramdisk/modules
+	$STRIP --strip-unneeded $(pwd)/anykernel/ramdisk/modules/wlan.ko
+	find $(pwd)/anykernel/ramdisk/modules -name '*.ko' -exec $(pwd)/out/scripts/sign-file sha512 $(pwd)/out/certs/signing_key.pem $(pwd)/out/certs/signing_key.x509 {} \;
 fi
 
-#	Name and push zip
-cd $(pwd)/anykernel
-# We don't need non treble anykernel
+
+if [[ ${BUILDFOR} == *"custom"* ]]; then
+	cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel/kernels/custom/
+fi
+
+
+
+# POST ZIP OR FAILURE
+cd anykernel
 rm -rf nontreble.sh
 mv treble.sh anykernel.sh
-if [[ ${SEMAPHORE_PROJECT_NAME} == *"oostest"* ]]; then 
-ZIPNAME="weebkernel_oos_r$SEMAPHORE_BUILD_NUMBER.zip"
 zip -r9 $ZIPNAME * -x README.md $ZIPNAME
 CHECKER=$(ls -l $ZIPNAME | awk '{print $5}')
-if (($((CHECKER / 1048576)) > 5));
-then
+
+if (($((CHECKER / 1048576)) > 5)); then
 	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build Version: <code>r$SEMAPHORE_BUILD_NUMBER</code>
 Compilation Time: <code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</code>
-ROM Support: <code>OxygenOS Oreo</code>
+ROM Support: <code>$BUILDFOR</code>
 <i>Uploading....</i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
 	curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
 else
 	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="The compiler decides to scream at @idkwhoiam322" -d chat_id=$CHAT_ID	
-fi;
 fi
-
-if [[ ${SEMAPHORE_PROJECT_NAME} == *"customtest"* ]]; then 
-ZIPNAME="weebkernel_custom_r$SEMAPHORE_BUILD_NUMBER.zip"
-zip -r9 $ZIPNAME * -x README.md $ZIPNAME
-CHECKER=$(ls -l $ZIPNAME | awk '{print $5}')
-if (($((CHECKER / 1048576)) > 5));
-then
-	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="Build Version: <code>r$SEMAPHORE_BUILD_NUMBER</code>
-Compilation Time: <code>$((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</code>
-ROM Support: <code>Custom ROMs</code>
-<i>Uploading....</i>" -d chat_id=$CHAT_ID -d parse_mode=HTML
-	curl -F chat_id="$CHAT_ID" -F document=@"$(pwd)/$ZIPNAME" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
-else
-	curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="The compiler decides to scream at @idkwhoiam322" -d chat_id=$CHAT_ID	
-fi;
-fi
-
-# Extra line here for OCD
-
-# Oh there were 2 lines
-# Oh 3
-# Ok I'll stop here
