00d6a9ba4e2d22b397075e9c7724c3db3d3484ee
idkwhoiam322 <snkm1999@gmail.com>
2019-01-11 17:12:49 +0530

    try new gcc 8 from RaphielGang repo https://github.com/RaphielGang/aarch64-linux-gnu-8.x

--- kernel.sh
+++ kernel.sh
@@ -54,8 +54,8 @@ if [[ ${COMPILER} == *"CLANG"* ]]; then
 fi
 
 if [[ ${COMPILER} == *"GCC"* ]]; then
-	export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-raph-linux-android-"
-	export STRIP="$(pwd)/gcc/bin/aarch64-raph-linux-android-strip"
+	export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-gnu-"
+	export STRIP="$(pwd)/gcc/bin/aarch64-linux-gnu-strip"
 
 		if [[ "$@" =~ "oos"* ]]; then 
 			export DEFCONFIG=weeb_defconfig
