cff5fe42a791907679b2959ae62796aeaccf4f6b
idkwhoiam322 <snkm1999@gmail.com>
2019-01-12 22:54:07 +0530

    Update AOSP Clang to 8.0.7

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ else
 	git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 clang
 	cd clang
 	ls
-	rm -rf clang-3289846 && rm -rf clang-4679922 && rm -rf clang-r328903 && rm -rf clang-r339409b && rm -rf clang-4679922 && rm -rf clang-3289846 && rm -rf clang-r328903 && rm -rf clang-r339409b && rm -rf clang-r344140b && rm -rf clang-r346389 && rm -rf clang-stable
+	rm -rf clang-3289846 && rm -rf clang-4679922 && rm -rf clang-r328903 && rm -rf clang-r339409b && rm -rf clang-4679922 && rm -rf clang-3289846 && rm -rf clang-r328903 && rm -rf clang-r339409b && rm -rf clang-r344140b && rm -rf clang-r346389 && rm -rf clang-r346389b && rm -rf clang-stable
 	ls
 	cd ..
 fi
