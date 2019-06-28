6c49dfe3e87df769246759e5701001cf136e3e4e
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-06-28 16:21:24 +0530

    gcc: clone with --depth=1 because I don't need full history

--- setup.sh
+++ setup.sh
@@ -8,11 +8,11 @@ git clone https://github.com/whoknowswhoiam/weebanykernel2.git -b pie anykernel
 
 
 if [[ "$@" =~ "gcc" ]]; then
-	git clone https://github.com/kdrag0n/aarch64-elf-gcc -b 9.x gcc
-	git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x gcc32
+	git clone https://github.com/kdrag0n/aarch64-elf-gcc -b 9.x --depth=1 gcc
+	git clone https://github.com/kdrag0n/arm-eabi-gcc -b 9.x --depth=1 gcc32
 else
-	git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x.git gcc
-	git clone https://github.com/RaphielGang/arm-linux-gnueabi-8.x.git gcc32
+	git clone https://github.com/RaphielGang/aarch64-linux-gnu-8.x.git --depth=1 gcc
+	git clone https://github.com/RaphielGang/arm-linux-gnueabi-8.x.git --depth=1 gcc32
 	git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 clang
 	cd clang
 	ls
