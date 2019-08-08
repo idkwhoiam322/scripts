7a08d43bd61bba7433dd5c33ba02189c243f4eb6
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-08-08 11:04:29 +0530

    prepare for wlan module as inline only

--- dronekernel.sh
+++ dronekernel.sh
@@ -65,7 +65,6 @@ Latest Commit: <code>$(git log --pretty=format:'%h : %s' -1)</code>
 curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build started for revision ${DRONE_BUILD_NUMBER}" -d chat_id=${KERNEL_CHAT_ID} -d parse_mode=HTML
 fi
 
-
 # compilation
 START=$(date +"%s")
 make O=out ARCH=arm64 $DEFCONFIG
@@ -77,23 +76,8 @@ fi
 END=$(date +"%s")
 DIFF=$((END - START))
 
-# prepare zip for oos
-if [[ ${BUILDFOR} == *"oos"* ]]; then
-	mkdir anykernel/modules
-	mkdir anykernel/modules/vendor
-	mkdir anykernel/modules/vendor/lib
-	mkdir anykernel/modules/vendor/lib/modules
+# prepare zip
 	cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel
-	cp $(pwd)/out/drivers/staging/qcacld-3.0/wlan.ko $(pwd)/anykernel/modules/vendor/lib/modules
-	mv $(pwd)/anykernel/modules/vendor/lib/modules/wlan.ko $(pwd)/anykernel/modules/vendor/lib/modules/qca_cld3_wlan.ko
-	${STRIP} --strip-unneeded $(pwd)/anykernel/modules/vendor/lib/modules/qca_cld3_wlan.ko
-fi
-
-# prepare zip for custom
-if [[ ${BUILDFOR} == *"custom"* ]] || [[ ${BUILDFOR} == *"hax"* ]]; then
-	cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel
-fi
-
 
 # POST ZIP OR FAILURE
 cd anykernel
@@ -112,47 +96,3 @@ fi
 rm -rf ${ZIPNAME} && rm -rf Image.gz-dtb && rm -rf modules
 cd ..
 rm -rf out
-
-	if [[ ${BUILDFOR} == *"custom"* ]]; then
-		export DEFCONFIG=weebomni_defconfig
-		export BUILDFOR=omni
-	if [[ "$@" =~ "stable"* ]]; then 
-		export ZIPNAME="${BUILDFOR}${VERSION}.zip"
-	else
-		export ZIPNAME="${KERNEL_BUILD_TYPE}-r${DRONE_BUILD_NUMBER}-${BUILDFOR}-$(git rev-parse --abbrev-ref HEAD)-$(grep "SUBLEVEL =" < Makefile | awk '{print $3}')$(grep "EXTRAVERSION =" < Makefile | awk '{print $3}').zip"
-	fi
-	START=$(date +"%s")
-	make O=out ARCH=arm64 $DEFCONFIG
-	if [[ "$@" =~ "gcc" ]]; then
-	make -j${KEBABS} O=out ARCH=arm64
-	else
-		make -j${KEBABS} O=out ARCH=arm64 CC="/drone/src/clang/clang-r353983e/bin/clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="/drone/src/gcc/bin/aarch64-linux-gnu-" CROSS_COMPILE_ARM32="/drone/src/gcc32/bin/arm-linux-gnueabi-"
-	fi
-	END=$(date +"%s")
-	DIFF=$((END - START))
-
-	# prepare zip for omni
-		mkdir anykernel/modules
-		mkdir anykernel/modules/system
-		mkdir anykernel/modules/system/lib
-		mkdir anykernel/modules/system/lib/modules
-		cp $(pwd)/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel
-		cp $(pwd)/out/drivers/staging/qcacld-3.0/wlan.ko $(pwd)/anykernel/modules/system/lib/modules
-		${STRIP} --strip-unneeded $(pwd)/anykernel/modules/system/lib/modules/wlan.ko
-
-	# final push
-	cd anykernel
-	zip -r9 ${ZIPNAME} * -x README.md ${ZIPNAME}
-	CHECKER=$(ls -l ${ZIPNAME} | awk '{print $5}')
-
-	if (($((CHECKER / 1048576)) > 5)); then
-		curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds for ${BUILDFOR}" -d chat_id=${CI_CHANNEL_ID} -d parse_mode=HTML
-		curl -F chat_id="${CI_CHANNEL_ID}" -F document=@"$(pwd)/${ZIPNAME}" https://api.telegram.org/bot${BOT_API_KEY}/sendDocument
-		curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build for ${BUILDFOR} pushed to CI Channel!" -d chat_id=${KERNEL_CHAT_ID}
-	else
-		curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="The compiler decides to scream at @idkwhoiam322 for ruining ${BUILDFOR}" -d chat_id=${KERNEL_CHAT_ID}
-		curl -s -X POST https://api.telegram.org/bot${BOT_API_KEY}/sendMessage -d text="Build for ${BUILDFOR} throwing err0rs yO" -d chat_id=${CI_CHANNEL_ID}
-		exit 1;
-	fi
-	rm -rf ${ZIPNAME} && rm -rf Image.gz-dtb && rm -rf modules
-fi
