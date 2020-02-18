c8b575b6d7d33dd9d23b2cb763e85b7ecff78f0a
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-02-18 21:44:07 +0530

    Revert "init submodules"
    This reverts commit 3e202a1ce42e2e2a89e246a1c8dd905f6f15137d.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ source env_vars.sh
 
 cd ${PROJECT_DIR} || exit
 
-git submodule update --init
-
 if [[ ${CI_ENVIRONMENT} == "SEMAPHORE_CI" ]]; then
 	sudo install-package --update-new autogen bc ccache git-core guile-2.0-libs \
 			libgc1c2 libopts25-dev bash gnupg
